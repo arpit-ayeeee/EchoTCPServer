6f5fcc043fa31616793f7625f373ed5ae38585ce
Arpit Jaiswal <arpitjaiswal282000@gmail.com>
2024-11-11 23:18:34 +0530

    Wrote a basic single-threaded TCP server

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/dicedb/dice/config"
+	"github.com/dicedb/dice/server"
+)
+
+// These are the commmand line flags which we'll give when running the server
+func setupFlags() {
+	flag.StringVar(&config.Host, "host", "0.0.0.0", "host for the dice server") //From which ip's we'll be accepting the connection, everyone
+	flag.IntVar(&config.Port, "port", 7379, "port for the dice server")         //our server will be running at this port
+	flag.Parse()
+}
+
+func main() {
+	setupFlags()
+	log.Println("rolling the dice 🎲")
+	server.RunSyncTCPServer() //running a synchronous tcp server
+}
